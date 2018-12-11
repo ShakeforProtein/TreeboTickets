75ef0ef7201ed106d25e7a2b2df4caad19d5260e
ShakeforProtein <github@taktimer.net>
2018-12-11 21:59:10 +1000

    Outstanding tickets fix

--- src/main/java/me/shakeforprotein/treebotickets/PlayerInput.java
+++ src/main/java/me/shakeforprotein/treebotickets/PlayerInput.java
@@ -37,8 +37,7 @@ public class PlayerInput implements Listener {
     public String staffsteps = "";
 
 
-
-   @EventHandler
+    @EventHandler
     public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent e) {
         Player p = e.getPlayer();
         String m = e.getMessage();
@@ -46,41 +45,38 @@ public class PlayerInput implements Listener {
 
         ConfigurationSection playersList = pl.getConfig().getConfigurationSection("players");
 
-        for(String item: playersList.getKeys(false)) {
-                if ((p.getName().equalsIgnoreCase(item)) && (pl.getConfig().getInt("players." + p.getName() + ".ticketstate") > 0)) {
-                    e.setCancelled(true);
-                    ticketState = pl.getConfig().getInt("players." + p.getName() + ".ticketstate");
-                    if (m.equalsIgnoreCase("cancel")) {
-                        ticketState = 0;
-                        pl.getConfig().set("players." + p.getName() + ".ticketstate", 0);
-                        pl.saveConfig();
-                        p.sendMessage("Ticket Canceled");
-                    }
-                    String tServer = pl.api.getServer().toString();
-                    UUID puuid = p.getUniqueId();
-                    String pname = p.getName();
-                    String type = "";
-                    String opened = LocalDateTime.now().toString();
-                    String status = "OPEN";
-                    String staff = "Unassigned";
-                    String pworld = p.getWorld().getName();
-                    String pServer = pl.getConfig().getString("serverName");
-                    Integer px = (int) p.getLocation().getX();
-                    Integer py = (int) p.getLocation().getY();
-                    Integer pz = (int) p.getLocation().getZ();
+        for (String item : playersList.getKeys(false)) {
+            if ((p.getName().equalsIgnoreCase(item)) && (pl.getConfig().getInt("players." + p.getName() + ".ticketstate") > 0)) {
+                e.setCancelled(true);
+                ticketState = pl.getConfig().getInt("players." + p.getName() + ".ticketstate");
+                if (m.equalsIgnoreCase("cancel")) {
+                    ticketState = 0;
+                    pl.getConfig().set("players." + p.getName() + ".ticketstate", 0);
+                    pl.saveConfig();
+                    p.sendMessage("Ticket Canceled");
+                }
+                String tServer = pl.api.getServer().toString();
+                UUID puuid = p.getUniqueId();
+                String pname = p.getName();
+                String type = "";
+                String opened = LocalDateTime.now().toString();
+                String status = "OPEN";
+                String staff = "Unassigned";
+                String pworld = p.getWorld().getName();
+                String pServer = pl.getConfig().getString("serverName");
+                Integer px = (int) p.getLocation().getX();
+                Integer py = (int) p.getLocation().getY();
+                Integer pz = (int) p.getLocation().getZ();
 
-                    if (ticketState == 1) {
-                        if(m.equals("1") || m.equals("2") || m.equals("3")){
+                if (ticketState == 1) {
+                    if (m.equals("1") || m.equals("2") || m.equals("3")) {
                         if (m.equals("1")) {
                             type = "Server";
-                        }
-                        else if (m.equals("2")) {
+                        } else if (m.equals("2")) {
                             type = "Griefer";
-                        }
-                        else if (m.equals("3")) {
+                        } else if (m.equals("3")) {
                             type = "Other";
                         }
-
 
 
                         pl.getConfig().set("players." + p.getName() + ".type", type);
@@ -92,72 +88,76 @@ public class PlayerInput implements Listener {
                         p.sendMessage("");
                         p.sendMessage("");
                         p.sendMessage("In your own words, please give a short explanation of the issue you are suffering");
-                        }
-                    }
-                    if (ticketState == 2) {
-                        description = m;
-                        pl.getConfig().set("players." + p.getName() + ".description", m);
-                        pl.getConfig().set("players." + p.getName() + ".ticketstate", 3);
-                        pl.saveConfig();
-
-                        p.sendMessage("");
-                        p.sendMessage("Your response - " + description);
-                        p.sendMessage("");
-                        p.sendMessage("");
-                        p.sendMessage("Briefly describe what steps you've taken to attempt to fix this issue yourself");
-                    }
-
-                    if (ticketState == 3) {
-                        usersteps = m;
-                        pl.getConfig().set("players." + p.getName() + ".usersteps", m);
-                        pl.getConfig().set("players." + p.getName() + ".ticketstate", 4);
-
-                        p.sendMessage("");
-                        p.sendMessage("Your response - " + usersteps);
-                        p.sendMessage("");
-                        p.sendMessage("");
-
-                        type = pl.getConfig().getString("players." + p.getName() + ".type");
-                        description = pl.getConfig().getString("players." + p.getName() + ".description");
-                        usersteps = pl.getConfig().getString("players." + p.getName() + ".usersteps");
-                        Integer severity = 0;
-                        String ticketData = "\"" + puuid + "\",\"" + p.getName() + "\",\"" + opened + "\",\"" + status + "\",\"" + staff + "\",\"" + pServer + "\",\"" + pworld + "\",\"" + px + "\",\"" + py + "\",\"" + pz + "\",\"" + type + "\",\"" + severity + "\",\"" + description + "\",\"" + usersteps + "\",\"" + staffsteps +"\"";
-                        pl.getConfig().set("players." + p.getName() + "lastQuery", pl.baseInsert.replace("XXXVALUESPLACEHOLDERXXX", ticketData));
-                        pl.getConfig().set("players." + p.getName() + ".ticketstate", 0);
-                        pl.saveConfig();
-                        p.sendMessage(pl.addTicketToDB(p, ticketData));
-                        p.sendMessage(ChatColor.GREEN + "Your ticket has been successfully submitted");
-
                     }
                 }
+                if (ticketState == 2) {
+                    description = m;
+                    pl.getConfig().set("players." + p.getName() + ".description", m);
+                    pl.getConfig().set("players." + p.getName() + ".ticketstate", 3);
+                    pl.saveConfig();
+
+                    p.sendMessage("");
+                    p.sendMessage("Your response - " + description);
+                    p.sendMessage("");
+                    p.sendMessage("");
+                    p.sendMessage("Briefly describe what steps you've taken to attempt to fix this issue yourself");
+                }
+
+                if (ticketState == 3) {
+                    usersteps = m;
+                    pl.getConfig().set("players." + p.getName() + ".usersteps", m);
+                    pl.getConfig().set("players." + p.getName() + ".ticketstate", 4);
+
+                    p.sendMessage("");
+                    p.sendMessage("Your response - " + usersteps);
+                    p.sendMessage("");
+                    p.sendMessage("");
+
+                    type = pl.getConfig().getString("players." + p.getName() + ".type");
+                    description = pl.getConfig().getString("players." + p.getName() + ".description");
+                    usersteps = pl.getConfig().getString("players." + p.getName() + ".usersteps");
+                    Integer severity = 0;
+                    String ticketData = "\"" + puuid + "\",\"" + p.getName() + "\",\"" + opened + "\",\"" + status + "\",\"" + staff + "\",\"" + pServer + "\",\"" + pworld + "\",\"" + px + "\",\"" + py + "\",\"" + pz + "\",\"" + type + "\",\"" + severity + "\",\"" + description + "\",\"" + usersteps + "\",\"" + staffsteps + "\"";
+                    pl.getConfig().set("players." + p.getName() + "lastQuery", pl.baseInsert.replace("XXXVALUESPLACEHOLDERXXX", ticketData));
+                    pl.getConfig().set("players." + p.getName() + ".ticketstate", 0);
+                    pl.saveConfig();
+                    p.sendMessage(pl.addTicketToDB(p, ticketData));
+                    p.sendMessage(ChatColor.GREEN + "Your ticket has been successfully submitted");
+
+                }
             }
+        }
     }
 
 
     @EventHandler
-    private void onPlayerJoin(PlayerJoinEvent e){
-        if (e.getPlayer().hasPermission(uc.requiredPermission)) {
-            if ((pl.getConfig().getString(e.getPlayer().getName()) == null) || ((pl.getConfig().getString(e.getPlayer().getName()) != null) && (pl.getConfig().getString(e.getPlayer().getName()).equalsIgnoreCase("false")))) {
-                uc.getCheckDownloadURL(e.getPlayer());
+    private void onPlayerJoin(PlayerJoinEvent e) {
+
+        if ((pl.getConfig().getString(e.getPlayer().getName()) == null) || ((pl.getConfig().getString(e.getPlayer().getName()) != null) && (pl.getConfig().getString(e.getPlayer().getName()).equalsIgnoreCase("false")))) {
+            if (e.getPlayer().hasPermission("tbtickets.view.any")) {
                 pl.staffStats(e.getPlayer());
+            }
+            if (e.getPlayer().hasPermission(uc.requiredPermission)) {
+                uc.getCheckDownloadURL(e.getPlayer());
                 pl.getConfig().set(e.getPlayer().getName(), "true");
                 Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(pl, new Runnable() {
                     public void run() {
                         pl.getConfig().set(e.getPlayer().getName(), "false");
                     }
                 }, 100L);
-            } else {
-                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(pl, new Runnable() {
-                    public void run() {
-                        try {
-                            pl.getConfig().set(e.getPlayer().getName(), null);
-                        } catch (NullPointerException e) {
-                        }
-                    }
-                }, 120L);
             }
+        } else {
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(pl, new Runnable() {
+                public void run() {
+                    try {
+                        pl.getConfig().set(e.getPlayer().getName(), null);
+                    } catch (NullPointerException e) {
+                    }
+                }
+            }, 120L);
         }
     }
+
 }
 
 
