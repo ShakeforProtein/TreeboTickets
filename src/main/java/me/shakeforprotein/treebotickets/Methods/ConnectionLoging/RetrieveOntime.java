d74b601bffb8132a4545448f39e3f141cac02cd5
ShakeforProtein <github@taktimer.net>
2019-02-10 00:10:45 +1000

    Added /discord Added more information to /onhere Added colours to /onhere last on timer. Added failsafe for /onhere Modified /onhere to run from console also.

--- src/main/java/me/shakeforprotein/treebotickets/Methods/ConnectionLoging/RetrieveOntime.java
+++ src/main/java/me/shakeforprotein/treebotickets/Methods/ConnectionLoging/RetrieveOntime.java
@@ -11,20 +11,22 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
 
 public class RetrieveOntime {
 
     private TreeboTickets pl;
-    public RetrieveOntime(TreeboTickets main){this.pl = main;}
 
-    public void retrieveOntime(String  p, CommandSender s) {
-        String query = "SELECT * FROM `"+ pl.ontimetable +"` WHERE `CurrentName`=\"" + p + "\" ORDER BY ID DESC LIMIT 1";
+    public RetrieveOntime(TreeboTickets main) {
+        this.pl = main;
+    }
+
+    public void retrieveOntime(String p, CommandSender s) {
+        String query = "SELECT * FROM `" + pl.ontimetable + "` WHERE `CurrentName`=\"" + p + "\" ORDER BY ID DESC LIMIT 1";
 
         ResultSet response;
         try {
             response = pl.connection.createStatement().executeQuery(query);
-            if(response.next()) {
+            if (response.next()) {
 
                 String uUID = (response.getString("UUID"));
                 String currentName = response.getString("CurrentName");
@@ -43,58 +45,68 @@ public class RetrieveOntime {
 
                 int oneDay = 1000 * 60 * 60 * 24;
 
-                if (currentTime - lastOff > (oneDay * 7)){
+                if (currentTime - lastOff > (oneDay * 7)) {
                     cColour = ChatColor.RED + "";
-                }
-                else if (currentTime - lastOff > (oneDay * 3)){
+                } else if (currentTime - lastOff > (oneDay * 3)) {
                     cColour = ChatColor.YELLOW + "";
-                }
-                else {
+                } else {
                     cColour = ChatColor.GREEN + "";
                 }
 
 
+                SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+                String fjDateStr = format.format(new Date(firstJoin));
+                String coDateStr = format.format(new Date(currentOn));
+                String llDateStr = format.format(new Date(lastOff));
+                String cTime = format.format(new Date(currentTime));
 
-                    SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-                    String dateString = format.format( new Date());
+                String fjdt = LocalDateTime.ofInstant(Instant.ofEpochMilli(firstJoin),  TimeZone.getDefault().toZoneId()).toString().split("T")[1] + " - " + fjDateStr;
+                String codt = LocalDateTime.ofInstant(Instant.ofEpochMilli(currentOn),  TimeZone.getDefault().toZoneId()).toString().split("T")[1] + " - " + coDateStr;
+                String lldt = LocalDateTime.ofInstant(Instant.ofEpochMilli(lastOff),    TimeZone.getDefault().toZoneId()).toString().split("T")[1] + " - " + llDateStr;
+                String cdt =  LocalDateTime.ofInstant(Instant.ofEpochMilli(currentTime), TimeZone.getDefault().toZoneId()).toString().split("T")[1] + " - " + cTime;
 
-                    String fjDateStr = format.format(new Date(firstJoin));
-                    String coDateStr = format.format(new Date(currentOn));
-                    String llDateStr = format.format(new Date(lastOff));
-                    String cTime = format.format(new Date(currentTime));
-
-                String fjdt = LocalDateTime.ofInstant(Instant.ofEpochMilli(firstJoin), TimeZone.getDefault().toZoneId()).toString().split("T")[1] + " - " + fjDateStr;
-                String codt = LocalDateTime.ofInstant(Instant.ofEpochMilli(currentOn), TimeZone.getDefault().toZoneId()).toString().split("T")[1] + " - " + coDateStr;
-                String lldt = LocalDateTime.ofInstant(Instant.ofEpochMilli(lastOff), TimeZone.getDefault().toZoneId()).toString().split("T")[1] + " - " + llDateStr;
-                String cdt = LocalDateTime.ofInstant(Instant.ofEpochMilli(currentTime), TimeZone.getDefault().toZoneId()).toString().split("T")[1] + " - " + llDateStr;
 
                 s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "Current Time  -  " + ChatColor.RESET + cdt + " - (" + cTime + ")");
                 s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "UUID  -  " + ChatColor.RESET + uUID);
-                s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "CurrentName  -  "+ ChatColor.RESET + currentName);
+                s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "CurrentName  -  " + ChatColor.RESET + currentName);
                 s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "OtherNames  -  " + ChatColor.RESET + otherNames);
                 s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "CurrentIP  -  " + ChatColor.RESET + currentIP);
                 s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "FirstJoin  -  " + ChatColor.RESET + "" + fjdt + " - (" + firstJoin + ")");
-                s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "LatestOntime  -  " + ChatColor.RESET + ChatColor.GREEN + codt +" - (" + currentOn + ")");
+                s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "LatestOntime  -  " + ChatColor.RESET + ChatColor.GREEN + codt + " - (" + currentOn + ")");
                 s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "LastLeft  -  " + ChatColor.RESET + cColour + lldt + " - (" + lastOff + ")");
                 s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "Offline for - " + ChatColor.RESET + cColour + formatTime(currentTime - lastOff));
                 s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "TotalOntime  -  " + ChatColor.RESET + ChatColor.GOLD + formatTime(totalOn));
 
+            } else {
+                s.sendMessage(ChatColor.RED + "No Data Matching that player name");
             }
-            else{s.sendMessage(ChatColor.RED + "No Data Matching that player name");}
         } catch (SQLException e) {
             System.out.println("Encountered " + e.toString() + " during retrieveOntime()");
             pl.makeLog(e);
         }
     }
 
-public static String formatTime(long millis) {
-    long seconds = Math.round((double) millis / 1000);
-    long hours = TimeUnit.SECONDS.toHours(seconds);
-    if (hours > 0)
-        seconds -= TimeUnit.HOURS.toSeconds(hours);
-    long minutes = seconds > 0 ? TimeUnit.SECONDS.toMinutes(seconds) : 0;
-    if (minutes > 0)
-        seconds -= TimeUnit.MINUTES.toSeconds(minutes);
-    return hours > 0 ? String.format("%02dH:%02dM:%02dS", hours, minutes, seconds) : String.format("%02d:%02d", minutes, seconds);
-}
+    public static String formatTime(long millis) {
+        long seconds = Math.round((double) millis / 1000);
+        long days = seconds / 86400;
+        seconds = seconds - (days * 86400);
+        long hours = seconds / 3600;
+        seconds = seconds - (hours * 3600);
+        long minutes = seconds / 60;
+        seconds = seconds - (minutes * 60);
+        String output = "";
+        if (days > 0) {
+            output += days + "D:" + hours + "H:" + minutes + "M:" + seconds + "S";
+        } else if (hours > 0) {
+            output += hours + "H:" + minutes + "M:" + seconds + "S";
+        } else if (minutes > 0) {
+            output += minutes + "M:" + seconds + "S";
+        } else if (seconds > 0) {
+            output += seconds + "S";
+        } else {
+            output += "ERROR ERROR ERROR ERROR ERROR";
+        }
+
+        return output;
+    }
 }
